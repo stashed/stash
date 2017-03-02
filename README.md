8c6425b606fb126d924a6065744ce0d749c67e22
tamal <tamal@appscode.com>
2017-03-02 15:50:39 -0800

    Clone skeleton from appscode/k3pc (#10)

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# kube-restic
+[Website](https://appscode.com) • [Slack](https://slack.appscode.com) • [Forum](https://discuss.appscode.com) • [Twitter](https://twitter.com/AppsCodeHQ)
+
+# restik
+Backup your Kubernetes Volumes
---
+++ hack/docker/setup.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+GOPATH=$(go env GOPATH)
+SRC=$GOPATH/src
+BIN=$GOPATH/bin
+ROOT=$GOPATH
+REPO_ROOT=$GOPATH/src/github.com/appscode/restik
+
+source "$REPO_ROOT/hack/libbuild/common/lib.sh"
+source "$REPO_ROOT/hack/libbuild/common/public_image.sh"
+
+APPSCODE_ENV=${APPSCODE_ENV:-dev}
+IMG=restik
+
+DIST=$GOPATH/src/github.com/appscode/restik/dist
+mkdir -p $DIST
+if [ -f "$DIST/.tag" ]; then
+	export $(cat $DIST/.tag | xargs)
+fi
+
+clean() {
+    pushd $GOPATH/src/github.com/appscode/restik/hack/docker
+    rm restik Dockerfile
+    popd
+}
+
+build_binary() {
+    pushd $GOPATH/src/github.com/appscode/restik
+    ./hack/builddeps.sh
+    ./hack/make.py build restik
+    detect_tag $DIST/.tag
+    popd
+}
+
+build_docker() {
+    pushd $GOPATH/src/github.com/appscode/restik/hack/docker
+    cp $DIST/restik/restik-linux-amd64 restik
+    chmod 755 restik
+
+    cat >Dockerfile <<EOL
+FROM alpine
+
+COPY restik /restik
+
+USER nobody:nobody
+ENTRYPOINT ["/restik"]
+EOL
+    local cmd="docker build -t appscode/$IMG:$TAG ."
+    echo $cmd; $cmd
+
+    rm restik Dockerfile
+    popd
+}
+
+build() {
+    build_binary
+    build_docker
+}
+
+docker_push() {
+    if [ "$APPSCODE_ENV" = "prod" ]; then
+        echo "Nothing to do in prod env. Are you trying to 'release' binaries to prod?"
+        exit 0
+    fi
+
+    if [[ "$(docker images -q appscode/$IMG:$TAG 2> /dev/null)" != "" ]]; then
+        docker_up $IMG:$TAG
+    fi
+}
+
+docker_release() {
+    if [ "$APPSCODE_ENV" != "prod" ]; then
+        echo "'release' only works in PROD env."
+        exit 1
+    fi
+    if [ "$TAG_STRATEGY" != "git_tag" ]; then
+        echo "'apply_tag' to release binaries and/or docker images."
+        exit 1
+    fi
+
+    if [[ "$(docker images -q appscode/$IMG:$TAG 2> /dev/null)" != "" ]]; then
+        docker push appscode/$IMG:$TAG
+    fi
+}
+
+source_repo $@
