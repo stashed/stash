20d6aa5e7f5e4c33b5a298b3ba73d7866f37dee9
sauman <sauman@appscode.com>
2017-03-28 12:18:43 +0600

    Check group regitration

--- api/install/install.go
+++ api/install/install.go
@@ -3,22 +3,25 @@ package install
 import (
 	aci "github.com/appscode/restik/api"
 	"k8s.io/kubernetes/pkg/apimachinery/announced"
+	"k8s.io/kubernetes/pkg/apimachinery/registered"
 	"k8s.io/kubernetes/pkg/util/sets"
 )
 
 func init() {
-	if err := announced.NewGroupMetaFactory(
-		&announced.GroupMetaFactoryArgs{
-			GroupName:                  aci.GroupName,
-			VersionPreferenceOrder:     []string{aci.V1beta1SchemeGroupVersion.Version},
-			ImportPrefix:               "github.com/appscode/restik/api",
-			RootScopedKinds:            sets.NewString("PodSecurityPolicy", "ThirdPartyResource"),
-			AddInternalObjectsToScheme: aci.AddToScheme,
-		},
-		announced.VersionToSchemeFunc{
-			aci.V1beta1SchemeGroupVersion.Version: aci.V1betaAddToScheme,
-		},
-	).Announce().RegisterAndEnable(); err != nil {
-		panic(err)
+	if !registered.IsRegistered(aci.GroupName) {
+		if err := announced.NewGroupMetaFactory(
+			&announced.GroupMetaFactoryArgs{
+				GroupName:                  aci.GroupName,
+				VersionPreferenceOrder:     []string{aci.V1beta1SchemeGroupVersion.Version},
+				ImportPrefix:               "github.com/appscode/restik/api",
+				RootScopedKinds:            sets.NewString("PodSecurityPolicy", "ThirdPartyResource"),
+				AddInternalObjectsToScheme: aci.AddToScheme,
+			},
+			announced.VersionToSchemeFunc{
+				aci.V1beta1SchemeGroupVersion.Version: aci.V1betaAddToScheme,
+			},
+		).Announce().RegisterAndEnable(); err != nil {
+			panic(err)
+		}
 	}
 }
