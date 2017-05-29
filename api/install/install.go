3da1f5137d020c51becccbe3091c8b3df681e0c0
sauman <sauman@appscode.com>
2017-05-29 13:06:20 +0600

    added-all

---
+++ api/install/install.go
@@ -0,0 +1,24 @@
+package install
+
+import (
+	"github.com/appscode/restik/api"
+	"k8s.io/kubernetes/pkg/apimachinery/announced"
+	"k8s.io/kubernetes/pkg/util/sets"
+)
+
+func init() {
+	if err := announced.NewGroupMetaFactory(
+		&announced.GroupMetaFactoryArgs{
+			GroupName:                  api.GroupName,
+			VersionPreferenceOrder:     []string{api.V1beta1SchemeGroupVersion.Version},
+			ImportPrefix:               "github.com/appscode/k8s-addons/api",
+			RootScopedKinds:            sets.NewString("PodSecurityPolicy", "ThirdPartyResource"),
+			AddInternalObjectsToScheme: api.AddToScheme,
+		},
+		announced.VersionToSchemeFunc{
+			api.V1beta1SchemeGroupVersion.Version: api.V1betaAddToScheme,
+		},
+	).Announce().RegisterAndEnable(); err != nil {
+		panic(err)
+	}
+}
