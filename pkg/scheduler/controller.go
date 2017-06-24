05d59439ced9f2c1010bc62264cfe3ea5a9655fc
tamal <tamal@appscode.com>
2017-06-24 06:52:06 -0700

    added-all

--- pkg/scheduler/controller.go
+++ pkg/scheduler/controller.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"reflect"
+	"strconv"
 	"time"
 
 	"github.com/appscode/log"
@@ -11,6 +12,8 @@ import (
 	scs "github.com/appscode/stash/client/clientset"
 	"github.com/appscode/stash/pkg/eventer"
 	shell "github.com/codeskyblue/go-sh"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/push"
 	"gopkg.in/robfig/cron.v2"
 	kerr "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -208,7 +211,7 @@ func (c *controller) configureScheduler() error {
 	return nil
 }
 
-func (c *controller) runOnce() error {
+func (c *controller) runOnce() (err error) {
 	select {
 	case <-c.locked:
 		log.Infof("Acquired lock for Restic %s@%s", c.opt.ResourceName, c.opt.ResourceNamespace)
@@ -217,20 +220,79 @@ func (c *controller) runOnce() error {
 		}()
 	default:
 		log.Warningf("Skipping backup schedule for Restic %s@%s", c.opt.ResourceName, c.opt.ResourceNamespace)
-		return nil
+		return
 	}
 
-	resource, err := c.StashClient.Restics(c.opt.ResourceNamespace).Get(c.opt.ResourceName)
+	var resource *sapi.Restic
+	resource, err = c.StashClient.Restics(c.opt.ResourceNamespace).Get(c.opt.ResourceName)
 	if kerr.IsNotFound(err) {
-		return nil
+		err = nil
+		return
 	} else if err != nil {
-		return err
+		return
 	}
 	if resource.ResourceVersion != c.resourceVersion {
 		return fmt.Errorf("Restic %s@%s version %s does not match expected version %s", resource.Name, resource.Namespace, resource.ResourceVersion, c.resourceVersion)
 	}
 
-	err = c.runBackup(resource)
+	startTime := metav1.Now()
+	defer func() {
+		endTime := metav1.Now()
+		totalRuns := prometheus.NewCounter(prometheus.CounterOpts{
+			Name: "testname1",
+			Help: "testhelp1",
+		})
+		totalRuns.Inc()
+		totalFails := prometheus.NewCounter(prometheus.CounterOpts{
+			Name: "testname1",
+			Help: "testhelp1",
+		})
+		totalDuration := prometheus.NewGauge(prometheus.GaugeOpts{
+			Name:        "testname2",
+			Help:        "testhelp2",
+			ConstLabels: prometheus.Labels{"foo": "bar", "dings": "bums"},
+		})
+		totalDuration.Set(endTime.Sub(startTime.Time).Seconds())
+		job := resource.Namespace + "-" + c.opt.Workload
+		push.Collectors(job, c.GroupingKeys(resource), c.opt.PushgatewayURL, totalRuns, totalFails, totalDuration)
+
+		resource.Status.BackupCount++
+		resource.Status.LastBackupTime = &startTime
+		if resource.Status.FirstBackupTime == nil {
+			resource.Status.FirstBackupTime = &startTime
+		}
+		resource.Status.LastBackupDuration = endTime.Sub(startTime.Time).String()
+		if _, e2 := c.StashClient.Restics(resource.Namespace).Update(resource); e2 != nil {
+			log.Errorf("Failed to update status for Restic %s@%s due to %s", resource.Name, resource.Namespace, err)
+		}
+	}()
+	for _, fg := range resource.Spec.FileGroups {
+		err = c.processFileGroup(resource, fg, strconv.FormatInt(startTime.Unix(), 10))
+		if err != nil {
+			return
+		}
+	}
+	return
+}
+
+func (c *controller) processFileGroup(resource *sapi.Restic, fg sapi.FileGroup, sessionID string) (err error) {
+	startTime := time.Now()
+	defer func() {
+		d := time.Now().Sub(startTime)
+		if c.opt.PushgatewayURL != "" {
+			metric2 := prometheus.NewGauge(prometheus.GaugeOpts{
+				Name:        "restic_backup_duration_seconds",
+				Help:        "Seconds taken to backup a filegroup",
+				ConstLabels: prometheus.Labels{"filegroup": sanitizeLabelValue(fg.Path), "session": sessionID},
+			})
+			metric2.Set(d.Seconds())
+
+			job := resource.Namespace + "-" + c.opt.Workload
+			push.Collectors(job, c.GroupingKeys(resource), c.opt.PushgatewayURL, metric2)
+		}
+	}()
+
+	err = c.takeBackup(resource, fg)
 	if err != nil {
 		log.Errorln("Backup operation failed for Reestic %s@%s due to %s", resource.Name, resource.Namespace, err)
 		backupFailure()
@@ -240,83 +302,62 @@ func (c *controller) runOnce() error {
 		c.recorder.Event(resource, apiv1.EventTypeNormal, eventer.EventReasonSuccessfulBackup, "Backup completed successfully.")
 	}
 
-	err = c.forgetSnapshots(resource)
+	err = c.forgetSnapshots(resource, fg)
 	if err != nil {
 		log.Errorln("Failed to forget old snapshots for Restic %s@%s due to %s", resource.Name, resource.Namespace, err)
 		c.recorder.Event(resource, apiv1.EventTypeNormal, eventer.EventReasonFailedToRetention, " ERROR: "+err.Error())
 	}
 
-	return nil
+	return
 }
 
-func (c *controller) runBackup(resource *sapi.Restic) error {
-	startTime := metav1.Now()
-	for _, fg := range resource.Spec.FileGroups {
-		args := []interface{}{"backup", fg.Path, "--force"}
-		// add tags if any
-		for _, tag := range fg.Tags {
-			args = append(args, "--tag")
-			args = append(args, tag)
-		}
-		err := c.sh.Command(resticExe, args...).Run()
-		if err != nil {
-			return err
-		}
+func (c *controller) takeBackup(resource *sapi.Restic, fg sapi.FileGroup) error {
+	args := []interface{}{"backup", fg.Path, "--force"}
+	// add tags if any
+	for _, tag := range fg.Tags {
+		args = append(args, "--tag")
+		args = append(args, tag)
 	}
-	endTime := metav1.Now()
+	return c.sh.Command(resticExe, args...).Run()
+}
 
-	resource.Status.BackupCount++
-	resource.Status.LastBackupTime = &startTime
-	if resource.Status.FirstBackupTime == nil {
-		resource.Status.FirstBackupTime = &startTime
+func (c *controller) forgetSnapshots(resource *sapi.Restic, fg sapi.FileGroup) error {
+	args := []interface{}{"forget"}
+	if fg.RetentionPolicy.KeepLastSnapshots > 0 {
+		args = append(args, sapi.KeepLast)
+		args = append(args, fg.RetentionPolicy.KeepLastSnapshots)
 	}
-	resource.Status.LastBackupDuration = endTime.Sub(startTime.Time).String()
-	_, err := c.StashClient.Restics(resource.Namespace).Update(resource)
+	if fg.RetentionPolicy.KeepHourlySnapshots > 0 {
+		args = append(args, sapi.KeepHourly)
+		args = append(args, fg.RetentionPolicy.KeepHourlySnapshots)
+	}
+	if fg.RetentionPolicy.KeepDailySnapshots > 0 {
+		args = append(args, sapi.KeepDaily)
+		args = append(args, fg.RetentionPolicy.KeepDailySnapshots)
+	}
+	if fg.RetentionPolicy.KeepWeeklySnapshots > 0 {
+		args = append(args, sapi.KeepWeekly)
+		args = append(args, fg.RetentionPolicy.KeepWeeklySnapshots)
+	}
+	if fg.RetentionPolicy.KeepMonthlySnapshots > 0 {
+		args = append(args, sapi.KeepMonthly)
+		args = append(args, fg.RetentionPolicy.KeepMonthlySnapshots)
+	}
+	if fg.RetentionPolicy.KeepYearlySnapshots > 0 {
+		args = append(args, sapi.KeepYearly)
+		args = append(args, fg.RetentionPolicy.KeepYearlySnapshots)
+	}
+	for _, tag := range fg.RetentionPolicy.KeepTags {
+		args = append(args, "--keep-tag")
+		args = append(args, tag)
+	}
+	for _, tag := range fg.Tags {
+		args = append(args, "--tag")
+		args = append(args, tag)
+	}
+	err := c.sh.Command(resticExe, args...).Run()
 	if err != nil {
-		log.Errorf("Failed to update status for Restic %s@%s due to %s", resource.Name, resource.Namespace, err)
-	}
-	return nil
-}
-
-func (c *controller) forgetSnapshots(r *sapi.Restic) error {
-	for _, fg := range r.Spec.FileGroups {
-		args := []interface{}{"forget"}
-		if fg.RetentionPolicy.KeepLastSnapshots > 0 {
-			args = append(args, sapi.KeepLast)
-			args = append(args, fg.RetentionPolicy.KeepLastSnapshots)
-		}
-		if fg.RetentionPolicy.KeepHourlySnapshots > 0 {
-			args = append(args, sapi.KeepHourly)
-			args = append(args, fg.RetentionPolicy.KeepHourlySnapshots)
-		}
-		if fg.RetentionPolicy.KeepDailySnapshots > 0 {
-			args = append(args, sapi.KeepDaily)
-			args = append(args, fg.RetentionPolicy.KeepDailySnapshots)
-		}
-		if fg.RetentionPolicy.KeepWeeklySnapshots > 0 {
-			args = append(args, sapi.KeepWeekly)
-			args = append(args, fg.RetentionPolicy.KeepWeeklySnapshots)
-		}
-		if fg.RetentionPolicy.KeepMonthlySnapshots > 0 {
-			args = append(args, sapi.KeepMonthly)
-			args = append(args, fg.RetentionPolicy.KeepMonthlySnapshots)
-		}
-		if fg.RetentionPolicy.KeepYearlySnapshots > 0 {
-			args = append(args, sapi.KeepYearly)
-			args = append(args, fg.RetentionPolicy.KeepYearlySnapshots)
-		}
-		for _, tag := range fg.RetentionPolicy.KeepTags {
-			args = append(args, "--keep-tag")
-			args = append(args, tag)
-		}
-		for _, tag := range fg.Tags {
-			args = append(args, "--tag")
-			args = append(args, tag)
-		}
-		err := c.sh.Command(resticExe, args...).Run()
-		if err != nil {
-			return err
-		}
+		return err
 	}
 	return nil
 }
--- pkg/scheduler/prometheus.go
+++ pkg/scheduler/prometheus.go
@@ -1,23 +1,39 @@
 package scheduler
 
 import (
+	"regexp"
+	"strings"
+
 	sapi "github.com/appscode/stash/api"
 	"github.com/prometheus/client_golang/prometheus/push"
 	"gopkg.in/ini.v1"
 )
+
+var (
+	invalidLabelCharRE = regexp.MustCompile(`[^a-zA-Z0-9_]`)
+)
+
+func sanitizeLabelName(name string) string {
+	return invalidLabelCharRE.ReplaceAllString(name, "_")
+}
+
+func sanitizeLabelValue(name string) string {
+	return strings.Replace(name, "/", "|", -1)
+}
 
 func (c *controller) GroupingKeys(resource *sapi.Restic) map[string]string {
 	labels := make(map[string]string)
 	if c.opt.PrefixHostname {
 		labels = push.HostnameGroupingKey()
 	}
-	labels["job"] = resource.Namespace + "/" + c.opt.Workload
 	labels["namespace"] = resource.Namespace
 	labels["stash_config"] = resource.Name
 	if cfg, err := ini.LooseLoad(c.opt.PodLabelsPath); err == nil {
 		for _, section := range cfg.Sections() {
 			for k, v := range section.KeysHash() {
-				labels["pod_"+k] = v
+				if k != "pod-template-hash" {
+					labels["pod_"+sanitizeLabelName(k)] = sanitizeLabelValue(v)
+				}
 			}
 		}
 	}
