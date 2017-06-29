a04dd903851dce59e3191987259d40d406969104
tamal <tamal@appscode.com>
2017-06-29 06:32:12 -0700

    added-all

--- pkg/controller/restics.go
+++ pkg/controller/restics.go
@@ -7,11 +7,14 @@ import (
 	acrt "github.com/appscode/go/runtime"
 	"github.com/appscode/log"
 	sapi "github.com/appscode/stash/api"
+	"github.com/appscode/stash/pkg/util"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/apimachinery/pkg/watch"
 	apiv1 "k8s.io/client-go/pkg/api/v1"
+	apps "k8s.io/client-go/pkg/apis/apps/v1beta1"
+	extensions "k8s.io/client-go/pkg/apis/extensions/v1beta1"
 	"k8s.io/client-go/tools/cache"
 )
 
@@ -62,39 +65,125 @@ func (c *Controller) WatchRestics() {
 }
 
 func (c *Controller) EnsureSidecar(old, new *sapi.Restic) {
-	sel, err := metav1.LabelSelectorAsSelector(&new.Spec.Selector)
+	var oldOpt, newOpt *metav1.ListOptions
+	if old != nil {
+		oldSelector, err := metav1.LabelSelectorAsSelector(&old.Spec.Selector)
+		if err != nil {
+			return
+		}
+		oldOpt = &metav1.ListOptions{LabelSelector: oldSelector.String()}
+	}
+
+	newSelector, err := metav1.LabelSelectorAsSelector(&new.Spec.Selector)
 	if err != nil {
 		return
 	}
-	opt := metav1.ListOptions{LabelSelector: sel.String()}
+	newOpt = &metav1.ListOptions{LabelSelector: newSelector.String()}
 
-	if resources, err := c.kubeClient.CoreV1().ReplicationControllers(new.Namespace).List(opt); err == nil {
-		for _, resource := range resources.Items {
-			go c.EnsureReplicationControllerSidecar(&resource, old, new)
+	{
+		oldObjs := make(map[string]apiv1.ReplicationController)
+		if oldOpt != nil {
+			if resources, err := c.kubeClient.CoreV1().ReplicationControllers(new.Namespace).List(*oldOpt); err == nil {
+				for _, resource := range resources.Items {
+					oldObjs[resource.Name] = resource
+				}
+			}
+		}
+
+		if resources, err := c.kubeClient.CoreV1().ReplicationControllers(new.Namespace).List(*newOpt); err == nil {
+			for _, resource := range resources.Items {
+				delete(oldObjs, resource.Name)
+				go c.EnsureReplicationControllerSidecar(&resource, old, new)
+			}
+		}
+		for _, resource := range oldObjs {
+			go c.EnsureReplicationControllerSidecarDeleted(&resource, old)
 		}
 	}
 
-	if resources, err := c.kubeClient.ExtensionsV1beta1().ReplicaSets(new.Namespace).List(opt); err == nil {
-		for _, resource := range resources.Items {
-			go c.EnsureReplicaSetSidecar(&resource, old, new)
+	{
+		oldObjs := make(map[string]extensions.ReplicaSet)
+		if oldOpt != nil {
+			if resources, err := c.kubeClient.ExtensionsV1beta1().ReplicaSets(new.Namespace).List(*oldOpt); err == nil {
+				for _, resource := range resources.Items {
+					oldObjs[resource.Name] = resource
+				}
+			}
+		}
+
+		if resources, err := c.kubeClient.ExtensionsV1beta1().ReplicaSets(new.Namespace).List(*newOpt); err == nil {
+			for _, resource := range resources.Items {
+				delete(oldObjs, resource.Name)
+				go c.EnsureReplicaSetSidecar(&resource, old, new)
+			}
+		}
+		for _, resource := range oldObjs {
+			go c.EnsureReplicaSetSidecarDeleted(&resource, old)
 		}
 	}
 
-	if resources, err := c.kubeClient.ExtensionsV1beta1().Deployments(new.Namespace).List(opt); err == nil {
-		for _, resource := range resources.Items {
-			go c.EnsureDeploymentExtensionSidecar(&resource, old, new)
+	{
+		oldObjs := make(map[string]extensions.Deployment)
+		if oldOpt != nil {
+			if resources, err := c.kubeClient.ExtensionsV1beta1().Deployments(new.Namespace).List(*oldOpt); err == nil {
+				for _, resource := range resources.Items {
+					oldObjs[resource.Name] = resource
+				}
+			}
+		}
+
+		if resources, err := c.kubeClient.ExtensionsV1beta1().Deployments(new.Namespace).List(*newOpt); err == nil {
+			for _, resource := range resources.Items {
+				delete(oldObjs, resource.Name)
+				go c.EnsureDeploymentExtensionSidecar(&resource, old, new)
+			}
+		}
+		for _, resource := range oldObjs {
+			go c.EnsureDeploymentExtensionSidecarDeleted(&resource, old)
 		}
 	}
 
-	if resources, err := c.kubeClient.AppsV1beta1().Deployments(new.Namespace).List(opt); err == nil {
-		for _, resource := range resources.Items {
-			go c.EnsureDeploymentAppSidecar(&resource, old, new)
+	{
+		if util.IsPreferredAPIResource(c.kubeClient, apps.SchemeGroupVersion.String(), "Deployment") {
+			oldObjs := make(map[string]apps.Deployment)
+			if oldOpt != nil {
+				if resources, err := c.kubeClient.AppsV1beta1().Deployments(new.Namespace).List(*oldOpt); err == nil {
+					for _, resource := range resources.Items {
+						oldObjs[resource.Name] = resource
+					}
+				}
+			}
+
+			if resources, err := c.kubeClient.AppsV1beta1().Deployments(new.Namespace).List(*newOpt); err == nil {
+				for _, resource := range resources.Items {
+					delete(oldObjs, resource.Name)
+					go c.EnsureDeploymentAppSidecar(&resource, old, new)
+				}
+			}
+			for _, resource := range oldObjs {
+				go c.EnsureDeploymentAppSidecarDeleted(&resource, old)
+			}
 		}
 	}
 
-	if resources, err := c.kubeClient.ExtensionsV1beta1().DaemonSets(new.Namespace).List(opt); err == nil {
-		for _, resource := range resources.Items {
-			go c.EnsureDaemonSetSidecar(&resource, old, new)
+	{
+		oldObjs := make(map[string]extensions.DaemonSet)
+		if oldOpt != nil {
+			if resources, err := c.kubeClient.ExtensionsV1beta1().DaemonSets(new.Namespace).List(*oldOpt); err == nil {
+				for _, resource := range resources.Items {
+					oldObjs[resource.Name] = resource
+				}
+			}
+		}
+
+		if resources, err := c.kubeClient.ExtensionsV1beta1().DaemonSets(new.Namespace).List(*newOpt); err == nil {
+			for _, resource := range resources.Items {
+				delete(oldObjs, resource.Name)
+				go c.EnsureDaemonSetSidecar(&resource, old, new)
+			}
+		}
+		for _, resource := range oldObjs {
+			go c.EnsureDaemonSetSidecarDeleted(&resource, old)
 		}
 	}
 }
