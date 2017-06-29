bbf726eeabca50c9fa23a5be1b87a7f4c39e0d67
Tamal Saha <tamal@appscode.com>
2017-06-29 01:08:23 -0700

    Set TMPDIR env var for restic (#115)

--- pkg/cmds/root.go
+++ pkg/cmds/root.go
@@ -13,7 +13,7 @@ func NewCmdStash(version string) *cobra.Command {
 	var rootCmd = &cobra.Command{
 		Use:   "stash",
 		Short: `Stash by AppsCode - Backup your Kubernetes Volumes`,
-		Long: `Stash is a Kubernetes operator for restic. For more information, visit here: https://github.com/appscode/stash/tree/master/docs`,
+		Long:  `Stash is a Kubernetes operator for restic. For more information, visit here: https://github.com/appscode/stash/tree/master/docs`,
 		PersistentPreRun: func(c *cobra.Command, args []string) {
 			c.Flags().VisitAll(func(flag *pflag.Flag) {
 				log.Printf("FLAG: --%s=%q", flag.Name, flag.Value)
