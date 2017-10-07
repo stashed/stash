05abff50b6240aaa612e2a2c538fa1431712debd
Tamal Saha <tamal@appscode.com>
2017-10-07 05:24:32 -0700

    Use WaitForCRDReady from kutil (#179)

--- test/e2e/framework/tpr.go
+++ test/e2e/framework/tpr.go
@@ -1,9 +1,9 @@
 package framework
 
 import (
+	"github.com/appscode/kutil"
 	sapi "github.com/appscode/stash/apis/stash"
 	sapi_v1alpha1 "github.com/appscode/stash/apis/stash/v1alpha1"
-	"github.com/appscode/stash/pkg/util"
 	. "github.com/onsi/gomega"
 	apiextensions "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -11,7 +11,7 @@ import (
 
 func (f *Framework) EventuallyCRD(name string) GomegaAsyncAssertion {
 	return Eventually(func() error {
-		return util.WaitForCRDReady(
+		return kutil.WaitForCRDReady(
 			f.KubeClient.CoreV1().RESTClient(),
 			[]*apiextensions.CustomResourceDefinition{
 				{
