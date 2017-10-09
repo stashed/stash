cccc23cf189ba91833d262fffd54ad5adbde6b99
tamal <tamal@appscode.com>
2017-10-09 08:38:00 -0700

    Register events api group

--- main.go
+++ main.go
@@ -7,7 +7,9 @@ import (
 	_ "github.com/appscode/stash/client/fake"
 	_ "github.com/appscode/stash/client/internalclientset/scheme"
 	"github.com/appscode/stash/pkg/cmds"
+	_ "github.com/appscode/stash/client/scheme"
 	_ "k8s.io/client-go/kubernetes/fake"
+	_ "k8s.io/client-go/pkg/api/v1"
 )
 
 func main() {
