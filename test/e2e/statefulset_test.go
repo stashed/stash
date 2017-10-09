d0c6b82a2cf11a26ec06f5e44827d756c367d6e1
tamal <tamal@appscode.com>
2017-10-09 05:19:18 -0700

    added-all

--- test/e2e/statefulset_test.go
+++ test/e2e/statefulset_test.go
@@ -1,12 +1,14 @@
 package e2e_test
 
 import (
+	apps_util "github.com/appscode/kutil/apps/v1beta1"
 	api "github.com/appscode/stash/apis/stash/v1alpha1"
 	"github.com/appscode/stash/pkg/util"
 	"github.com/appscode/stash/test/e2e/framework"
 	. "github.com/appscode/stash/test/e2e/matcher"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	apiv1 "k8s.io/client-go/pkg/api/v1"
 	apps "k8s.io/client-go/pkg/apis/apps/v1beta1"
 )
@@ -62,6 +64,142 @@ var _ = Describe("StatefulSet", func() {
 			By("Waiting for backup event")
 			f.EventualEvent(restic.ObjectMeta).Should(WithTransform(f.CountSuccessfulBackups, BeNumerically(">=", 1)))
 		}
+
+		shouldBackupExistingStatefulSet = func() {
+			By("Creating repository Secret " + cred.Name)
+			err = f.CreateSecret(cred)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Creating service " + svc.Name)
+			err = f.CreateService(svc)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Creating StatefulSet " + ss.Name)
+			err = f.CreateStatefulSet(ss)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Creating restic " + restic.Name)
+			err = f.CreateRestic(restic)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Waiting for sidecar")
+			f.EventuallyStatefulSet(ss.ObjectMeta).Should(HaveSidecar(util.StashContainer))
+
+			By("Waiting for backup to complete")
+			f.EventuallyRestic(restic.ObjectMeta).Should(WithTransform(func(r *api.Restic) int64 {
+				return r.Status.BackupCount
+			}, BeNumerically(">=", 1)))
+
+			By("Waiting for backup event")
+			f.EventualEvent(restic.ObjectMeta).Should(WithTransform(f.CountSuccessfulBackups, BeNumerically(">=", 1)))
+		}
+
+		shouldStopBackup = func() {
+			By("Creating repository Secret " + cred.Name)
+			err = f.CreateSecret(cred)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Creating restic " + restic.Name)
+			err = f.CreateRestic(restic)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Creating service " + svc.Name)
+			err = f.CreateService(svc)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Creating StatefulSet " + ss.Name)
+			err = f.CreateStatefulSet(ss)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Waiting for sidecar")
+			f.EventuallyStatefulSet(ss.ObjectMeta).Should(HaveSidecar(util.StashContainer))
+
+			By("Waiting for backup to complete")
+			f.EventuallyRestic(restic.ObjectMeta).Should(WithTransform(func(r *api.Restic) int64 {
+				return r.Status.BackupCount
+			}, BeNumerically(">=", 1)))
+
+			By("Deleting restic " + restic.Name)
+			f.DeleteRestic(restic.ObjectMeta)
+
+			f.EventuallyStatefulSet(ss.ObjectMeta).ShouldNot(HaveSidecar(util.StashContainer))
+		}
+
+		shouldStopBackupIfLabelChanged = func() {
+			By("Creating repository Secret " + cred.Name)
+			err = f.CreateSecret(cred)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Creating restic " + restic.Name)
+			err = f.CreateRestic(restic)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Creating service " + svc.Name)
+			err = f.CreateService(svc)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Creating StatefulSet " + ss.Name)
+			err = f.CreateStatefulSet(ss)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Waiting for sidecar")
+			f.EventuallyStatefulSet(ss.ObjectMeta).Should(HaveSidecar(util.StashContainer))
+
+			By("Waiting for backup to complete")
+			f.EventuallyRestic(restic.ObjectMeta).Should(WithTransform(func(r *api.Restic) int64 {
+				return r.Status.BackupCount
+			}, BeNumerically(">=", 1)))
+
+			By("Removing labels of StatefulSet " + ss.Name)
+			_, err = apps_util.PatchStatefulSet(f.KubeClient, &ss, func(in *apps.StatefulSet) *apps.StatefulSet {
+				in.Labels = map[string]string{
+					"app": "unmatched",
+				}
+				return in
+			})
+			Expect(err).NotTo(HaveOccurred())
+
+			f.EventuallyStatefulSet(ss.ObjectMeta).ShouldNot(HaveSidecar(util.StashContainer))
+		}
+
+		shouldStopBackupIfSelectorChanged = func() {
+			By("Creating repository Secret " + cred.Name)
+			err = f.CreateSecret(cred)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Creating restic " + restic.Name)
+			err = f.CreateRestic(restic)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Creating service " + svc.Name)
+			err = f.CreateService(svc)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Creating StatefulSet " + ss.Name)
+			err = f.CreateStatefulSet(ss)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Waiting for sidecar")
+			f.EventuallyStatefulSet(ss.ObjectMeta).Should(HaveSidecar(util.StashContainer))
+
+			By("Waiting for backup to complete")
+			f.EventuallyRestic(restic.ObjectMeta).Should(WithTransform(func(r *api.Restic) int64 {
+				return r.Status.BackupCount
+			}, BeNumerically(">=", 1)))
+
+			By("Change selector of Restic " + restic.Name)
+			err = f.UpdateRestic(restic.ObjectMeta, func(in *api.Restic) *api.Restic {
+				in.Spec.Selector = metav1.LabelSelector{
+					MatchLabels: map[string]string{
+						"app": "unmatched",
+					},
+				}
+				return in
+			})
+			Expect(err).NotTo(HaveOccurred())
+
+			f.EventuallyStatefulSet(ss.ObjectMeta).ShouldNot(HaveSidecar(util.StashContainer))
+		}
 	)
 
 	Describe("Creating restic for", func() {
@@ -78,6 +216,7 @@ var _ = Describe("StatefulSet", func() {
 				restic = f.ResticForLocalBackend()
 			})
 			It(`should backup new StatefulSet`, shouldBackupNewStatefulSet)
+			It(`should backup existing StatefulSet`, shouldBackupExistingStatefulSet)
 		})
 
 		Context(`"S3" backend`, func() {
@@ -86,6 +225,7 @@ var _ = Describe("StatefulSet", func() {
 				restic = f.ResticForS3Backend()
 			})
 			It(`should backup new StatefulSet`, shouldBackupNewStatefulSet)
+			It(`should backup existing StatefulSet`, shouldBackupExistingStatefulSet)
 		})
 
 		Context(`"GCS" backend`, func() {
@@ -94,6 +234,7 @@ var _ = Describe("StatefulSet", func() {
 				restic = f.ResticForGCSBackend()
 			})
 			It(`should backup new StatefulSet`, shouldBackupNewStatefulSet)
+			It(`should backup existing StatefulSet`, shouldBackupExistingStatefulSet)
 		})
 
 		Context(`"Azure" backend`, func() {
@@ -102,6 +243,7 @@ var _ = Describe("StatefulSet", func() {
 				restic = f.ResticForAzureBackend()
 			})
 			It(`should backup new StatefulSet`, shouldBackupNewStatefulSet)
+			It(`should backup existing StatefulSet`, shouldBackupExistingStatefulSet)
 		})
 
 		Context(`"Swift" backend`, func() {
@@ -110,6 +252,83 @@ var _ = Describe("StatefulSet", func() {
 				restic = f.ResticForSwiftBackend()
 			})
 			It(`should backup new StatefulSet`, shouldBackupNewStatefulSet)
+			It(`should backup existing StatefulSet`, shouldBackupExistingStatefulSet)
+		})
+	})
+
+	Describe("Changing StatefulSet labels", func() {
+		AfterEach(func() {
+			f.DeleteStatefulSet(ss.ObjectMeta)
+			f.DeleteService(svc.ObjectMeta)
+			f.DeleteRestic(restic.ObjectMeta)
+			f.DeleteSecret(cred.ObjectMeta)
+		})
+		BeforeEach(func() {
+			cred = f.SecretForLocalBackend()
+			restic = f.ResticForLocalBackend()
+		})
+		It(`should stop backup`, shouldStopBackupIfLabelChanged)
+	})
+
+	Describe("Changing Restic selector", func() {
+		AfterEach(func() {
+			f.DeleteStatefulSet(ss.ObjectMeta)
+			f.DeleteService(svc.ObjectMeta)
+			f.DeleteRestic(restic.ObjectMeta)
+			f.DeleteSecret(cred.ObjectMeta)
+		})
+		BeforeEach(func() {
+			cred = f.SecretForLocalBackend()
+			restic = f.ResticForLocalBackend()
+		})
+		It(`should stop backup`, shouldStopBackupIfSelectorChanged)
+	})
+
+	Describe("Deleting restic for", func() {
+		AfterEach(func() {
+			f.DeleteStatefulSet(ss.ObjectMeta)
+			f.DeleteService(svc.ObjectMeta)
+			f.DeleteSecret(cred.ObjectMeta)
+		})
+
+		Context(`"Local" backend`, func() {
+			BeforeEach(func() {
+				cred = f.SecretForLocalBackend()
+				restic = f.ResticForLocalBackend()
+			})
+			It(`should stop backup`, shouldStopBackup)
+		})
+
+		Context(`"S3" backend`, func() {
+			BeforeEach(func() {
+				cred = f.SecretForS3Backend()
+				restic = f.ResticForS3Backend()
+			})
+			It(`should stop backup`, shouldStopBackup)
+		})
+
+		Context(`"GCS" backend`, func() {
+			BeforeEach(func() {
+				cred = f.SecretForGCSBackend()
+				restic = f.ResticForGCSBackend()
+			})
+			It(`should stop backup`, shouldStopBackup)
+		})
+
+		Context(`"Azure" backend`, func() {
+			BeforeEach(func() {
+				cred = f.SecretForAzureBackend()
+				restic = f.ResticForAzureBackend()
+			})
+			It(`should stop backup`, shouldStopBackup)
+		})
+
+		Context(`"Swift" backend`, func() {
+			BeforeEach(func() {
+				cred = f.SecretForSwiftBackend()
+				restic = f.ResticForSwiftBackend()
+			})
+			It(`should stop backup`, shouldStopBackup)
 		})
 	})
 })
