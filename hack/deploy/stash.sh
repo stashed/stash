44c0136aa4db78fabb69b884d65a9f8145d98c33
tamal <tamal@appscode.com>
2018-02-09 10:43:15 -0800

    Merge uninstall script into the stash.sh script

--- hack/deploy/stash.sh
+++ hack/deploy/stash.sh
@@ -13,6 +13,7 @@ export STASH_ENABLE_INITIALIZER=false
 export STASH_ENABLE_ADMISSION_WEBHOOK=false
 export STASH_DOCKER_REGISTRY=appscode
 export STASH_IMAGE_PULL_SECRET=
+export STASH_UNINSTALL=0
 
 show_help() {
     echo "stash.sh - install stash operator"
@@ -26,8 +27,9 @@ show_help() {
     echo "    --docker-registry              docker registry used to pull stash images (default: appscode)"
     echo "    --image-pull-secret            name of secret used to pull stash operator images"
     echo "    --run-on-master                run stash operator on master"
-    echo "    --enable-apiserver     configure admission webhook for stash CRDs"
+    echo "    --enable-admission-webhook     configure admission webhook for stash CRDs"
     echo "    --enable-initializer           configure stash operator as workload initializer"
+    echo "    --uninstall                    uninstall stash"
 }
 
 while test $# -gt 0; do
@@ -59,7 +61,7 @@ while test $# -gt 0; do
             export STASH_IMAGE_PULL_SECRET="name: '$secret'"
             shift
             ;;
-        --enable-apiserver)
+        --enable-admission-webhook)
             export STASH_ENABLE_ADMISSION_WEBHOOK=true
             shift
             ;;
@@ -76,12 +78,29 @@ while test $# -gt 0; do
             export STASH_RUN_ON_MASTER=1
             shift
             ;;
+        --uninstall)
+            export STASH_UNINSTALL=1
+            shift
+            ;;
         *)
             show_help
             exit 1
             ;;
     esac
 done
+
+if [ "$STASH_UNINSTALL" -eq 1 ]; then
+    kubectl delete deployment -l app=stash --namespace $STASH_NAMESPACE
+    kubectl delete service -l app=stash --namespace $STASH_NAMESPACE
+    kubectl delete secret -l app=stash --namespace $STASH_NAMESPACE
+    kubectl delete apiservice -l app=stash --namespace $STASH_NAMESPACE
+    # Delete RBAC objects, if --rbac flag was used.
+    kubectl delete serviceaccount -l app=stash --namespace $STASH_NAMESPACE
+    kubectl delete clusterrolebindings -l app=stash --namespace $STASH_NAMESPACE
+    kubectl delete clusterrole -l app=stash --namespace $STASH_NAMESPACE
+
+    exit 0
+fi
 
 env | sort | grep STASH*
 echo ""
@@ -90,55 +109,48 @@ echo "checking kubeconfig context"
 kubectl config current-context || { echo "Set a context (kubectl use-context <context>) out of the following:"; echo; kubectl config get-contexts; exit 1; }
 echo ""
 
-if [ "$STASH_ENABLE_ADMISSION_WEBHOOK" = true ]; then
-    # ref: https://stackoverflow.com/a/27776822/244009
-    case "$(uname -s)" in
-        Darwin)
-            curl -fsSL -o onessl https://github.com/appscode/onessl/releases/download/0.1.0/onessl-darwin-amd64
-            chmod +x onessl
-            export ONESSL=./onessl
-            ;;
+# ref: https://stackoverflow.com/a/27776822/244009
+case "$(uname -s)" in
+    Darwin)
+        curl -fsSL -o onessl https://github.com/appscode/onessl/releases/download/0.1.0/onessl-darwin-amd64
+        chmod +x onessl
+        export ONESSL=./onessl
+        ;;
 
-        Linux)
-            curl -fsSL -o onessl https://github.com/appscode/onessl/releases/download/0.1.0/onessl-linux-amd64
-            chmod +x onessl
-            export ONESSL=./onessl
-            ;;
+    Linux)
+        curl -fsSL -o onessl https://github.com/appscode/onessl/releases/download/0.1.0/onessl-linux-amd64
+        chmod +x onessl
+        export ONESSL=./onessl
+        ;;
 
-        CYGWIN*|MINGW32*|MSYS*)
-            curl -fsSL -o onessl.exe https://github.com/appscode/onessl/releases/download/0.1.0/onessl-windows-amd64.exe
-            chmod +x onessl.exe
-            export ONESSL=./onessl.exe
-            ;;
-        *)
-            echo 'other OS'
-            ;;
-    esac
+    CYGWIN*|MINGW32*|MSYS*)
+        curl -fsSL -o onessl.exe https://github.com/appscode/onessl/releases/download/0.1.0/onessl-windows-amd64.exe
+        chmod +x onessl.exe
+        export ONESSL=./onessl.exe
+        ;;
+    *)
+        echo 'other OS'
+        ;;
+esac
 
-    # create necessary TLS certificates:
-    # - a local CA key and cert
-    # - a webhook server key and cert signed by the local CA
-    $ONESSL create ca-cert
-    $ONESSL create server-cert server --domains=stash-operator.$STASH_NAMESPACE.svc
-    export SERVICE_SERVING_CERT_CA=$(cat ca.crt | $ONESSL base64)
-    export TLS_SERVING_CERT=$(cat server.crt | $ONESSL base64)
-    export TLS_SERVING_KEY=$(cat server.key | $ONESSL base64)
-    export KUBE_CA=$($ONESSL get kube-ca | $ONESSL base64)
-    rm -rf $ONESSL ca.crt ca.key server.crt server.key
+# create necessary TLS certificates:
+# - a local CA key and cert
+# - a webhook server key and cert signed by the local CA
+$ONESSL create ca-cert
+$ONESSL create server-cert server --domains=stash-operator.$STASH_NAMESPACE.svc
+export SERVICE_SERVING_CERT_CA=$(cat ca.crt | $ONESSL base64)
+export TLS_SERVING_CERT=$(cat server.crt | $ONESSL base64)
+export TLS_SERVING_KEY=$(cat server.key | $ONESSL base64)
+export KUBE_CA=$($ONESSL get kube-ca | $ONESSL base64)
+rm -rf $ONESSL ca.crt ca.key server.crt server.key
 
-    curl -fsSL https://raw.githubusercontent.com/appscode/stash/0.7.0-alpha.0/hack/deploy/admission/operator.yaml | envsubst | kubectl apply -f -
-else
-    curl -fsSL https://raw.githubusercontent.com/appscode/stash/0.7.0-alpha.0/hack/deploy/operator.yaml | envsubst | kubectl apply -f -
-fi
+curl -fsSL https://raw.githubusercontent.com/appscode/stash/0.7.0-alpha.0/hack/deploy/operator.yaml | envsubst | kubectl apply -f -
 
 if [ "$STASH_ENABLE_RBAC" = true ]; then
     kubectl create serviceaccount $STASH_SERVICE_ACCOUNT --namespace $STASH_NAMESPACE
     kubectl label serviceaccount $STASH_SERVICE_ACCOUNT app=stash --namespace $STASH_NAMESPACE
     curl -fsSL https://raw.githubusercontent.com/appscode/stash/0.7.0-alpha.0/hack/deploy/rbac-list.yaml | envsubst | kubectl auth reconcile -f -
 
-    if [ "$STASH_ENABLE_ADMISSION_WEBHOOK" = true ]; then
-        curl -fsSL https://raw.githubusercontent.com/appscode/stash/0.7.0-alpha.0/hack/deploy/admission/rbac-list.yaml | envsubst | kubectl auth reconcile -f -
-    fi
 fi
 
 if [ "$STASH_RUN_ON_MASTER" -eq 1 ]; then
@@ -148,4 +160,8 @@ fi
 
 if [ "$STASH_ENABLE_INITIALIZER" = true ]; then
     kubectl apply -f https://raw.githubusercontent.com/appscode/stash/0.7.0-alpha.0/hack/deploy/initializer.yaml
+fi
+
+if [ "$STASH_ENABLE_ADMISSION_WEBHOOK" = true ]; then
+    curl -fsSL https://raw.githubusercontent.com/appscode/stash/0.7.0-alpha.0/hack/deploy/admission.yaml | envsubst | kubectl apply -f -
 fi
