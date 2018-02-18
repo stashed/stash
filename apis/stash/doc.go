053a540003e9763de702c6ef7cb136254159f1b3
tamal <tamal@appscode.com>
2018-02-17 19:26:59 -0800

    Remove internal types

--- apis/stash/doc.go
+++
@@ -1,5 +0,0 @@
-// Package stash is the internal version of the API.
-
-// +k8s:deepcopy-gen=package,register
-// +groupName=stash.appscode.com
-package stash
--- apis/stash/v1alpha1/crd.go
+++ apis/stash/v1alpha1/crd.go
@@ -1,7 +1,7 @@
 package v1alpha1
 
 import (
-	sapi "github.com/appscode/stash/apis/stash"
+	"github.com/appscode/stash/apis/stash"
 	apiextensions "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -9,17 +9,17 @@ import (
 func (c Restic) CustomResourceDefinition() *apiextensions.CustomResourceDefinition {
 	return &apiextensions.CustomResourceDefinition{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:   sapi.ResourceTypeRestic + "." + SchemeGroupVersion.Group,
+			Name:   ResourceTypeRestic + "." + SchemeGroupVersion.Group,
 			Labels: map[string]string{"app": "stash"},
 		},
 		Spec: apiextensions.CustomResourceDefinitionSpec{
-			Group:   sapi.GroupName,
+			Group:   stash.GroupName,
 			Version: SchemeGroupVersion.Version,
 			Scope:   apiextensions.NamespaceScoped,
 			Names: apiextensions.CustomResourceDefinitionNames{
-				Singular:   sapi.ResourceNameRestic,
-				Plural:     sapi.ResourceTypeRestic,
-				Kind:       sapi.ResourceKindRestic,
+				Singular:   ResourceNameRestic,
+				Plural:     ResourceTypeRestic,
+				Kind:       ResourceKindRestic,
 				ShortNames: []string{"rst"},
 			},
 		},
@@ -29,17 +29,17 @@ func (c Restic) CustomResourceDefinition() *apiextensions.CustomResourceDefiniti
 func (c Recovery) CustomResourceDefinition() *apiextensions.CustomResourceDefinition {
 	return &apiextensions.CustomResourceDefinition{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:   sapi.ResourceTypeRecovery + "." + SchemeGroupVersion.Group,
+			Name:   ResourceTypeRecovery + "." + SchemeGroupVersion.Group,
 			Labels: map[string]string{"app": "stash"},
 		},
 		Spec: apiextensions.CustomResourceDefinitionSpec{
-			Group:   sapi.GroupName,
+			Group:   stash.GroupName,
 			Version: SchemeGroupVersion.Version,
 			Scope:   apiextensions.NamespaceScoped,
 			Names: apiextensions.CustomResourceDefinitionNames{
-				Singular:   sapi.ResourceNameRecovery,
-				Plural:     sapi.ResourceTypeRecovery,
-				Kind:       sapi.ResourceKindRecovery,
+				Singular:   ResourceNameRecovery,
+				Plural:     ResourceTypeRecovery,
+				Kind:       ResourceKindRecovery,
 				ShortNames: []string{"rec"},
 			},
 		},
