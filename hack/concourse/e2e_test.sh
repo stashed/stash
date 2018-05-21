bb19413e8f1d987a867dfed8edc132b040553c38
tahsinrahman <tahsynx@gmail.com>
2018-05-21 19:09:23 +0600

    install pharmer

--- hack/concourse/e2e_test.sh
+++ hack/concourse/e2e_test.sh
@@ -24,6 +24,23 @@ function cleanup {
 }
 trap cleanup EXIT
 
+# install kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl &> /dev/null
+chmod +x ./kubectl
+mv ./kubectl /bin/kubectl
+
+# install onessl
+curl -fsSL -o onessl https://github.com/kubepack/onessl/releases/download/0.3.0/onessl-linux-amd64 \
+  && chmod +x onessl \
+  && mv onessl /usr/local/bin/
+
+# install pharmer
+pushd /tmp
+curl -LO https://cdn.appscode.com/binaries/pharmer/0.1.0-rc.3/pharmer-linux-amd64
+chmod +x pharmer-linux-amd64
+mv pharmer-linux-amd64 /bin/pharmer
+popd
+
 pharmer create credential --from-file=cred.json --provider=DigitalOcean cred
 pharmer create cluster $NAME --provider=digitalocean --zone=nyc3 --nodes=2gb=1 --credential-uid=cred --kubernetes-version=v1.9.0
 pharmer apply $NAME
