377aff7e70b47a25ba903ac1fb6e56eeb5f76c02
tahsinrahman <tahsynx@gmail.com>
2018-05-21 20:18:07 +0600

    install dependencies before running tests

--- hack/concourse/e2e_test.sh
+++ hack/concourse/e2e_test.sh
@@ -2,6 +2,9 @@
 
 set -x -e
 
+apt-get update &> /dev/null
+apt-get install -y git python python-pip &> /dev/null
+
 mkdir -p $GOPATH/src/github.com/appscode
 cp -r stash $GOPATH/src/github.com/appscode
 cd $GOPATH/src/github.com/appscode/stash
