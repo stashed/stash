74e6f4aa413be1b093d069a95fbedc68d9e9cd6a
Md. Tahsin Rahman <tahsynx@gmail.com>
2018-05-23 00:19:17 +0600

    Concourse (#486)

--- hack/concourse/unit_test.sh
+++ hack/concourse/unit_test.sh
@@ -2,12 +2,14 @@
 
 set -x -e
 
+apt-get update &> /dev/null
+apt-get install -y git python python-pip &> /dev/null
+
 mkdir -p $GOPATH/src/github.com/appscode
 cp -r stash $GOPATH/src/github.com/appscode
 pushd $GOPATH/src/github.com/appscode/stash
 
 ./hack/builddeps.sh
-./hack/make.py
 ./hack/make.py test unit
 
 popd
