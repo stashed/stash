5ea70e5af6abf0747b0eef2cea5d90c0294ea906
Tamal Saha <tamal@appscode.com>
2018-07-23 23:42:15 -0700

    Fix extended apiserver issues with Kubernetes 1.11

--- apis/repositories/install/install.go
+++ apis/repositories/install/install.go
@@ -17,6 +17,7 @@ limitations under the License.
 package install
 
 import (
+	"github.com/appscode/stash/apis/repositories"
 	"github.com/appscode/stash/apis/repositories/v1alpha1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
@@ -24,6 +25,7 @@ import (
 
 // Install registers the API group and adds types to a scheme
 func Install(scheme *runtime.Scheme) {
+	utilruntime.Must(repositories.AddToScheme(scheme))
 	utilruntime.Must(v1alpha1.AddToScheme(scheme))
 	utilruntime.Must(scheme.SetVersionPriority(v1alpha1.SchemeGroupVersion))
 }
