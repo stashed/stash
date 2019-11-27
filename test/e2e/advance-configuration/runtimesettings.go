a3d77e6ff3bf0622f033ff243527ab130f4947a5
hossainemruz <hossainemruz@gmail.com>
2019-11-27 18:54:23 +0600

    use busybox

---
+++ test/e2e/advance-configuration/runtimesettings.go
@@ -0,0 +1,99 @@
+/*
+Copyright The Stash Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package advance_configuration
+
+import (
+	"fmt"
+
+	"stash.appscode.dev/stash/apis"
+	"stash.appscode.dev/stash/apis/stash/v1beta1"
+	"stash.appscode.dev/stash/test/e2e/framework"
+	. "stash.appscode.dev/stash/test/e2e/matcher"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var _ = Describe("Deployment", func() {
+
+	var f *framework.Invocation
+
+	BeforeEach(func() {
+		f = framework.NewInvocation()
+	})
+
+	AfterEach(func() {
+		err := f.CleanupTestResources()
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	Context("Deployment", func() {
+		Context("With NICE and IONICE", func() {
+			It("should Backup & Restore successfully", func() {
+				// Deploy a Deployment
+				deployment, err := f.DeployDeployment(fmt.Sprintf("source-deployment1-%s", f.App()), int32(1))
+				Expect(err).NotTo(HaveOccurred())
+
+				// Generate Sample Data
+				sampleData, err := f.GenerateSampleData(deployment.ObjectMeta, apis.KindDeployment)
+				Expect(err).NotTo(HaveOccurred())
+
+				// Setup a Minio Repository
+				repo, err := f.SetupMinioRepository()
+				Expect(err).NotTo(HaveOccurred())
+				f.AppendToCleanupList(repo)
+
+				// Setup workload Backup
+				backupConfig, err := f.SetupWorkloadBackup(deployment.ObjectMeta, repo, apis.KindDeployment, nil, nil)
+				Expect(err).NotTo(HaveOccurred())
+
+				// Take an Instant Backup the Sample Data
+				backupSession, err := f.TakeInstantBackup(backupConfig.ObjectMeta)
+				Expect(err).NotTo(HaveOccurred())
+
+				By("Verifying that BackupSession has succeeded")
+				completedBS, err := f.StashClient.StashV1beta1().BackupSessions(backupSession.Namespace).Get(backupSession.Name, metav1.GetOptions{})
+				Expect(err).NotTo(HaveOccurred())
+				Expect(completedBS.Status.Phase).Should(Equal(v1beta1.BackupSessionSucceeded))
+
+				// Simulate disaster scenario. Delete the data from source PVC
+				By("Deleting sample data from source Deployment")
+				err = f.CleanupSampleDataFromWorkload(deployment.ObjectMeta, apis.KindDeployment)
+				Expect(err).NotTo(HaveOccurred())
+
+				// Restore the backed up data
+				By("Restoring the backed up data in the original Deployment")
+				restoreSession, err := f.SetupRestoreProcess(deployment.ObjectMeta, repo, apis.KindDeployment, nil, nil)
+				Expect(err).NotTo(HaveOccurred())
+
+				By("Verifying that RestoreSession succeeded")
+				completedRS, err := f.StashClient.StashV1beta1().RestoreSessions(restoreSession.Namespace).Get(restoreSession.Name, metav1.GetOptions{})
+				Expect(err).NotTo(HaveOccurred())
+				Expect(completedRS.Status.Phase).Should(Equal(v1beta1.RestoreSessionSucceeded))
+
+				// Get restored data
+				restoredData := f.RestoredData(deployment.ObjectMeta, apis.KindDeployment)
+
+				// Verify that restored data is same as the original data
+				By("Verifying restored data is same as the original data")
+				Expect(restoredData).Should(BeSameAs(sampleData))
+			})
+		})
+	})
+
+})
