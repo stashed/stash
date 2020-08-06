a47444ba4ee9c3ff5fa717f122c648153950463a
1gtm <1gtm@appscode.com>
2020-08-06 07:54:36 +0000

    Update Kubernetes v1.18.3 dependencies
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- vendor/kmodules.xyz/client-go/api/v1/certificates.go
+++ vendor/kmodules.xyz/client-go/api/v1/certificates.go
@@ -25,34 +25,41 @@ type CertificateSpec struct {
 	// +optional
 	SecretName string `json:"secretName,omitempty" protobuf:"bytes,2,opt,name=secretName"`
 
+	// KeyEncoding is the private key cryptography standards (PKCS)
+	// for this certificate's private key to be encoded in.
+	// If provided, allowed values are "pkcs1" and "pkcs8".
+	// If KeyEncoding is not specified, then PKCS#1 will be used by default.
+	// +optional
+	KeyEncoding string `json:"keyEncoding,omitempty" protobuf:"bytes,3,opt,name=keyEncoding"`
+
 	// Full X509 name specification (https://golang.org/pkg/crypto/x509/pkix/#Name).
 	// +optional
-	Subject *X509Subject `json:"subject,omitempty" protobuf:"bytes,3,opt,name=subject"`
+	Subject *X509Subject `json:"subject,omitempty" protobuf:"bytes,4,opt,name=subject"`
 
 	// Certificate default Duration
 	// +optional
-	Duration *metav1.Duration `json:"duration,omitempty" protobuf:"bytes,4,opt,name=duration"`
+	Duration *metav1.Duration `json:"duration,omitempty" protobuf:"bytes,5,opt,name=duration"`
 
 	// Certificate renew before expiration duration
 	// +optional
-	RenewBefore *metav1.Duration `json:"renewBefore,omitempty" protobuf:"bytes,5,opt,name=renewBefore"`
+	RenewBefore *metav1.Duration `json:"renewBefore,omitempty" protobuf:"bytes,6,opt,name=renewBefore"`
 
 	// DNSNames is a list of subject alt names to be used on the Certificate.
 	// +optional
-	DNSNames []string `json:"dnsNames,omitempty" protobuf:"bytes,6,rep,name=dnsNames"`
+	DNSNames []string `json:"dnsNames,omitempty" protobuf:"bytes,7,rep,name=dnsNames"`
 
 	// IPAddresses is a list of IP addresses to be used on the Certificate
 	// +optional
-	IPAddresses []string `json:"ipAddresses,omitempty" protobuf:"bytes,7,rep,name=ipAddresses"`
+	IPAddresses []string `json:"ipAddresses,omitempty" protobuf:"bytes,8,rep,name=ipAddresses"`
 
 	// URISANs is a list of URI Subject Alternative Names to be set on this
 	// Certificate.
 	// +optional
-	URISANs []string `json:"uriSANs,omitempty" protobuf:"bytes,8,rep,name=uriSANs"`
+	URISANs []string `json:"uriSANs,omitempty" protobuf:"bytes,9,rep,name=uriSANs"`
 
 	// EmailSANs is a list of email subjectAltNames to be set on the Certificate.
 	// +optional
-	EmailSANs []string `json:"emailSANs,omitempty" protobuf:"bytes,9,rep,name=emailSANs"`
+	EmailSANs []string `json:"emailSANs,omitempty" protobuf:"bytes,10,rep,name=emailSANs"`
 }
 
 // X509Subject Full X509 name specification
