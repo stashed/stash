52657e8fbfd8335666b08448482dd1fdc6274953
1gtm <1gtm@appscode.com>
2020-08-08 07:40:18 +0000

    Update Kubernetes v1.18.3 dependencies
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- vendor/kmodules.xyz/client-go/api/v1/openapi_generated.go
+++ vendor/kmodules.xyz/client-go/api/v1/openapi_generated.go
@@ -228,7 +228,6 @@ func schema_kmodulesxyz_client_go_api_v1_TLSConfig(ref common.ReferenceCallback)
 						},
 					},
 				},
-				Required: []string{"issuerRef"},
 			},
 		},
 		Dependencies: []string{
