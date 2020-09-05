d7ef6c3887bf5c3b518bbb8f11ebb01fe6e606d4
Tamal Saha <tamal@appscode.com>
2020-09-04 21:09:12 -0700

    Use AppsCode Community License
    This allows both commercial and non-commercial organizations use the Community Edition of this software.
    
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- test/e2e/hooks/post_backup.go
+++ test/e2e/hooks/post_backup.go
@@ -1,11 +1,11 @@
 /*
 Copyright AppsCode Inc. and Contributors
 
-Licensed under the PolyForm Noncommercial License 1.0.0 (the "License");
+Licensed under the AppsCode Community License 1.0.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    https://github.com/appscode/licenses/raw/1.0.0/PolyForm-Noncommercial-1.0.0.md
+    https://github.com/appscode/licenses/raw/1.0.0/AppsCode-Community-1.0.0.md
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
