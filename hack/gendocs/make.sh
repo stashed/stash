dc70cedd93bc308d8e54c54217872177ba6a8ea1
Tamal Saha <tamal@appscode.com>
2024-01-06 04:18:33 -0800

    Update gendocs script
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/gendocs/make.sh
+++ hack/gendocs/make.sh
@@ -14,6 +14,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-pushd $GOPATH/src/stash.appscode.dev/stash/hack/gendocs
+set -eou pipefail
+
+SCRIPT_ROOT=$(realpath $(dirname "${BASH_SOURCE[0]}")/../..)
+SCRIPT_NAME=$(basename "${BASH_SOURCE[0]}")
+pushd $SCRIPT_ROOT/hack/gendocs
+
+# http://redsymbol.net/articles/bash-exit-traps/
+function cleanup() {
+    popd
+}
+trap cleanup EXIT
+
 go run main.go
-popd
